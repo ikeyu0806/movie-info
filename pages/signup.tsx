74daa01f0f8b5f3b4360cfaedea0ea741d78828c
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-12 19:44:08 +0900

    connect to backend, add password to signup form

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,6 +4,13 @@ import axios from 'axios'
 import Layout from '../components/Layout'
 
 const SignUp = () => {
+  const ExecSignUp = () => {
+    axios.post('http://localhost:8000/login'), {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+  }
   return (
     <>
       <Layout title="映画情報サービス">
@@ -38,6 +45,32 @@ const SignUp = () => {
           </div>
 
           <div className="field">
+            <label className="label">パスワード</label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input" type="password1" placeholder="パスワード"></input>
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">パスワード（確認）</label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input" type="password2" placeholder="パスワード（確認）"></input>
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+          </div>
+
+          <div className="field">
             <div className="control">
               <label className="checkbox">
                 <input type="checkbox" />
@@ -48,7 +81,7 @@ const SignUp = () => {
 
           <div className="field is-grouped confirm-buttons">
             <div className="control">
-              <button className="button is-link">Submit</button>
+              <button className="button is-link" onClick={ExecSignUp}>Submit</button>
             </div>
             <div className="control">
               <button className="button is-link is-light">Cancel</button>
