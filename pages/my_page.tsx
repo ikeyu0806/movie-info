b7eb965563105f73bb4464f73ceb1638094f02d3
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-25 17:26:42 +0900

    add login condition to mypage

--- pages/my_page.tsx
+++ pages/my_page.tsx
@@ -1,16 +1,36 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { useEffect, useState } from "react"
+import { CurrentUser } from '../interfaces/User'
 
-const MyPage = () => (
-  <Layout title="MY Page">
-    <h1>MyPage</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>ホームに戻る</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const MyPage = () => {
+  const [currentUser, setCurrentUser] = useState<CurrentUser>({id: 0, token: "", name: "", email: ""});
+
+  useEffect(() => {
+    const currentUserInfo = localStorage.getItem('current_user');
+    if (currentUserInfo) {
+      setCurrentUser(JSON.parse(currentUserInfo));
+    }
+  }, []);
+
+  return (
+    <Layout title="MY Page">  
+      <h1>MyPage</h1>
+      {currentUser.name
+        ?
+          <div>
+            <p>This is the about page</p> 
+            <a className="button is-danger">
+              ログアウト
+            </a>
+          </div>
+        : 
+          <p>ログインしてください</p>}
+        <Link href="/">
+          <a>ホームに戻る</a>
+        </Link>
+    </Layout>
+  )
+}
 
 export default MyPage
