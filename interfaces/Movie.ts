747041958a061d93653553becda8d8c5ee55afb7
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-26 20:40:11 +0900

    add property to Movie type

--- interfaces/Movie.ts
+++ interfaces/Movie.ts
@@ -2,9 +2,9 @@ export interface Movie {
   id: number
   title: string
   poster_path: string
-  // backdrop_path: string
-  // overview: string
-  // homepage: string
-  // vote_average: number
-  // vote_count: number
+  backdrop_path: string
+  overview: string
+  homepage: string
+  vote_average: number
+  vote_count: number
 }
--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -5,24 +5,42 @@ import axios from 'axios'
 import { Movie } from '../../interfaces/Movie'
 
 const MovieDetail = () => {
-  const [movie, setMovie] = useState<Movie>({id: 0, title: '', poster_path: ''});
+  const [movie, setMovie] = useState<Movie>({ id: 0,
+                                              title: '',
+                                              poster_path: '',
+                                              backdrop_path: '',
+                                              overview: '',
+                                              homepage: '',
+                                              vote_average: 0,
+                                              vote_count: 0
+                                            });
 
   useEffect(() => {
     async function fetchMovie() {
       const Movie = await axios.get(
         'https://api.themoviedb.org/3/movie/' + window.location.pathname.split('/')[2] + '?api_key='　+ process.env.tmdbApi + '&language=ja',
       );
-      setMovie({id: Movie.data.id, title: Movie.data.title, poster_path: Movie.data.poster_path})
-      // console.log(Movie.data.id)
-      // console.log(movie)
+      setMovie({id: Movie.data.id,
+                title: Movie.data.title,
+                poster_path: Movie.data.poster_path,
+                backdrop_path: Movie.data.backdrop_path,
+                overview: Movie.data.overview,
+                homepage: Movie.data.homepage,
+                vote_average: Movie.data.vote_average,
+                vote_count: Movie.data.vote_count
+              })
     }
     fetchMovie();
   }, []);
   return (
     <Layout title="映画情報サービス">
-      <h1>Hi</h1>
       <p>{movie.title}</p>
       <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}></img>
+      <img src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path}></img>
+      <p>{movie.overview}</p>
+      <p>{movie.homepage}</p>
+      <p>{movie.vote_average}</p>
+      <p>{movie.vote_count}</p>
     </Layout>
   )
 }
