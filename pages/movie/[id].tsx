3b3c5615fc4c1adc7c32436d7072c80d07be0be6
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-26 20:31:30 +0900

    show title and poster to movie detail page

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -1,12 +1,30 @@
 import Layout from '../../components/Layout'
 import { useState, useEffect } from "react"
+import axios from 'axios'
 
-const Movie = () => {
+import { Movie } from '../../interfaces/Movie'
+
+const MovieDetail = () => {
+  const [movie, setMovie] = useState<Movie>({id: 0, title: '', poster_path: ''});
+
+  useEffect(() => {
+    async function fetchMovie() {
+      const Movie = await axios.get(
+        'https://api.themoviedb.org/3/movie/' + window.location.pathname.split('/')[2] + '?api_key='　+ process.env.tmdbApi + '&language=ja',
+      );
+      setMovie({id: Movie.data.id, title: Movie.data.title, poster_path: Movie.data.poster_path})
+      // console.log(Movie.data.id)
+      // console.log(movie)
+    }
+    fetchMovie();
+  }, []);
   return (
     <Layout title="映画情報サービス">
       <h1>Hi</h1>
+      <p>{movie.title}</p>
+      <img src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}></img>
     </Layout>
   )
 }
 
-export default Movie;
+export default MovieDetail;
