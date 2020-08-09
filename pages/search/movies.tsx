041c5b3f8295e3262817325c4047aeb70048e200
ikeyu0806 <ikeyu0806@gmail.com>
2020-08-09 10:34:18 +0900

    add input field

--- pages/search/movies.tsx
+++ pages/search/movies.tsx
@@ -4,9 +4,27 @@ import React from "react"
 const SearchMoviesPage = (): JSX.Element => {
 
   return (
+    <>
     <Layout title="映画情報サービス">
-      <h1>Hi</h1>
+      <div className="field has-addons has-addons-centered search-field">
+        <div className="control is-expanded">
+          <input className="input" type="text" placeholder="映画名を入力してください" />
+        </div>
+        <div className="control">
+          <a className="button is-info">
+            Search
+          </a>
+        </div>
+      </div>
     </Layout>
+    <style jsx>{`
+      .search-field {
+        margin-top: 20px;
+        margin-left: 400px;
+        margin-right: 400px;
+      }
+    `}</style>
+    </>
   )
 }
 
